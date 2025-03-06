2c9568fb9fb3deafa83b519b4b6e1ed881fd8f7c
dmcclane1986 <dmcclane1986@gmail.com>
2025-03-05 18:11:25 -0600

    made printout

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from stats import count_words
 from stats import count_letters
 from stats import alpha_only
 
+
 def get_book_text(filepath):
     file_contents= None
     with open(filepath) as file:
@@ -15,12 +16,20 @@ def main():
     word_count = count_words(book)
     
 
-   # print(f"{word_count} words found in the document")
+    
     letter_count = count_letters(book)
     alpha_dict =alpha_only(letter_count)
     sorted_dit = dict(sorted(alpha_dict.items(), key=lambda item: item[1] , reverse=True))
-    print(sorted_dit)
+    
 
+    print("=========== BOOKBOT ============")
+    print(f"Analyzing book found at {book_location}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for a in sorted_dit:
+        print(f"{a}: {sorted_dit[a]}")
+    print("============= END ===============")
 
 
 main()
